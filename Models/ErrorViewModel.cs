94a59e2675ba6f830312126ea7b2389615270cc8
Sotiris Kakavoulis <sotiriskakavoulis@gmail.com>
2023-01-24 00:14:30 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductionRead_02.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
